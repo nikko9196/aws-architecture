8ba82f8663ded36d99eaf56cf3e9b8a07d8dbd14
Nikko Pham <nikkopham@Nikkos-MacBook-Air.local>
2024-01-17 00:36:30 +1300

    Commit 3: 3 Lambda Function with EventBus - Done

--- Part IV/app/handler.py
+++ Part IV/app/handler.py
@@ -10,6 +10,10 @@ import pydantic as py
 from PIL import Image
 import PIL.Image
 
+# Environment variables
+S3_PROCESSED_BUCKET = os.getenv("S3_PROCESSED_BUCKET")
+
+# Clients
 s3_client = boto3.client("s3")
 
 
@@ -24,13 +28,12 @@ def resize_image(image_path, resized_path, max_width=200):
 
             resized_image.save(resized_path)
             print(f"resize image size {resized_image.size}")
-            
+
         else:
             resized_image = image.resize((image.width, image.height))
             resized_image.save(resized_path)
             print(f"resize image size {resized_image.size}")
 
-        
 
 def process_image_function(event, context):
     try:
@@ -43,7 +46,9 @@ def process_image_function(event, context):
             # original = uploads/kitty-cat-kitten-pet-45201.jpg
 
             # destination = thumbnails/kitty-cat-kitten-pet-45201.jpg
-            print(f"The resize name is s3://{bucket_name}/{destination_s3_path}")
+            print(
+                f"The resize name is s3://{S3_PROCESSED_BUCKET}/{destination_s3_path}"
+            )
 
             tmpkey = original_s3_path.replace("/", "")
             # adding uuid4 to make sure we process an unique object locally to Lambda
@@ -56,10 +61,23 @@ def process_image_function(event, context):
 
             resize_image(local_download_path, local_upload_path)
 
-            s3_client.upload_file(local_upload_path, bucket_name, destination_s3_path)
+            s3_client.upload_file(
+                local_upload_path, S3_PROCESSED_BUCKET, destination_s3_path
+            )
+
+            return {"status": "SUCCEEDED"}
 
     except Exception as e:
         print(f"Error: {e}")
+        return {"status": "FAILED"}
+
+
+def notify_resize_success_function(event, context):
+    print("SUCCEED")
+
+
+def notify_resize_fail_function(event, context):
+    print("FAILED")
 
 
 # def process_image_function(event, context):
